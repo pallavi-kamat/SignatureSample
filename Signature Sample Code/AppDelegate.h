d045adb5a439d61ee3194af83d88914884b810a1
Pallavi Kamat <pallavi@weboniselab.com>
2013-11-07 15:23:19 +0530

    Initial Commit

---
+++ Signature Sample Code/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Signature Sample Code
+//
+//  Created by Mac-4 on 07/11/13.
+//  Copyright (c) 2013 Pallavi Kamat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
